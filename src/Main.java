56ad9f623a7da18b36dc20f60f89551060acb677
aavidsoft-developer <aavidsoft.developer@gmail.com>
2023-08-22 16:01:28 +0530

    Array of generic type

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Stack<String> names = new Stack<>(4);
+        names.push("Vishal");
+        names.push("Sap");
+        System.out.println(names.pop());
+
+        Stack s = new Stack(4);
+
+    }
+}
